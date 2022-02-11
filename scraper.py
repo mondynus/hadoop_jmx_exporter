6929d792771dc31510e3a256110514b78b0d2c7a
Marian Vajdecka <mondynus@gmail.com>
2022-02-11 12:31:40 +0100

    Update scraper.py
    kerberos suport

--- scraper.py
+++ scraper.py
@@ -4,6 +4,12 @@
 import threading
 import requests
 
+import re
+import socket
+import sys
+from requests_kerberos import HTTPKerberosAuth
+
+
 from utils import get_module_logger
 
 
@@ -14,14 +20,37 @@ class Scraper(threading.Thread):
     def __init__(self, url, result):
         super(Scraper, self).__init__()
         self.name = "thread-%s" % url
-        self.url = url
+        self.url = self.fixurl(url)
         self.result = result
+    
+    def validate_ip(self,ip_str):
+      reg = r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+      if re.match(reg, ip_str):
+        return True
+      else:
+        return False
+
+    def fixurl(self,url):
+        address = url.replace("http://","")
+        part_address=address.split(":")
+
+        if self.validate_ip(part_address[0]):
+          host = socket.gethostbyaddr(part_address[0])
+          host=host[0]+":"+str(part_address[1])
+        else:
+          host=address
+
+        return "http://"+host
+    
 
     def run(self):
         result = []
         try:
             s = requests.session()
-            response = s.get(self.url, timeout=5)
+            if "8088" in self.url:
+              response = s.get(self.url, timeout=5)
+            else:
+              response = s.get(self.url, timeout=5,auth=HTTPKerberosAuth())
         except Exception as e:
             logger.warning("Get {0} failed, error: {1}.".format(self.url, str(e)))
         else:
